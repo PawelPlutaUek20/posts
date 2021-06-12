4ae453ddf28be2b433d6c383729dc6a8e3cb5a85
PawelPlutaUek20 <pawelpluta58@gmail.com>
2021-06-12 21:58:40 +0200

    added endpoints

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
+const dbConn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: 'test'
 })
+
+dbConn.connect(function(err) {
+    if (err) throw err;
+    console.log("Database Connected!");
+  });
+
+module.exports = dbConn;
--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const db = require('./db');
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', async(req, res) => {
     const results = await db.promise().query('SELECT * FROM posts');
+    res.status(200).send(results[0]);
+})
+
+app.get('/:id', async(req,res) => {
+    const results = await db.promise().query('SELECT * FROM posts WHERE id = ?', [req.params.id]);
+    res.status(200).send(results[0]);
+})
+
+app.delete('/:id', async(req, res) => {
+    const results = await db.promise().query('DELETE FROM posts WHERE id = ?', [req.params.id]);
+    res.status(200).send(results[0]);
+})
+
+app.post('/', async(req, res) => {
+    const { title, lead, content } = req.body;
+    const results = await db.promise().query(
+        'INSERT INTO posts (title, lead, content, updatedAt) VALUES (?, ?, ?, ?)',
+        [title, lead, content, null]
+    );
+    res.status(200).send(results[0]);
+})
+
+app.put('/:id', async(req, res) => {
+    const { title, lead, content } = req.body;
+    const results = await db.promise().query(
+        'UPDATE posts SET title = ?, lead = ?, content = ?, updatedAt = ? WHERE id = ?',
+        [title, lead, content, new Date(), req.params.id]
+    );
     res.status(200).send(results[0]);
 })
 
